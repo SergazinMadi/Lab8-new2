cd62b9c544bbeb2e6eb2b768bc4bcfd2299a1716
Sausaq <nadikz4444@gmail.com>
2025-04-07 14:31:47 +0500

    Add a service and activity with buttons to start and stop the service and navigate to the activity.

--- app/src/main/java/serg/madi/lab8_new2/MainActivity.java
+++ app/src/main/java/serg/madi/lab8_new2/MainActivity.java
@@ -8,17 +8,40 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.core.content.ContextCompat;
+
+
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+
+
 public class MainActivity extends AppCompatActivity {
+    private Intent serviceIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        serviceIntent = new Intent(this, MyService.class);
+    }
+
+    public void startService(View view) {
+        ContextCompat.startForegroundService(MainActivity.this, serviceIntent);
+    }
+    public void nextActivity(View view) {
+        startActivity(new Intent(MainActivity.this, MainActivity2.class));
+    }
+    public void stopService(View view) {
+        stopService(serviceIntent);
     }
 }
+
