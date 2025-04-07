dfe97449cf89247a15da05412d51c446f247cae8
Sausaq <nadikz4444@gmail.com>
2025-04-07 14:32:08 +0500

    Add a new activity MainActivity2.

---
+++ app/src/main/java/serg/madi/lab8_new2/MainActivity2.java
@@ -0,0 +1,13 @@
+package serg.madi.lab8_new2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
